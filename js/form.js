fcbf1e3a5bb00b6ef36fe234050bc544d0395b9d
VladoVV <wq2127@gmail.com>
2017-01-30 10:00:52 +0200

    Кексобукинг урок 3 задание1 3/4

--- js/form.js
+++ js/form.js
@@ -2,26 +2,35 @@
 
 var pinElements = document.querySelectorAll('.pin'); // выбираем все элементы с классом pin
 var pinElement = document.querySelector('.pin'); // выбираем первый попавшейся элемент с классом pin
-
-//---отслеживаем событие клик на первом попавшемся элементе с классом пин---//
-pinElement.addEventListener('click', function () {
-  
-  for (var i = 0; i < pinElements.length; i++) {
-    pinElements[i].classList.remove('pin--active'); // перебираем все элементы с классом pin и удаляем класс pin--active
-  }
-  
-  this.parentNode.classList.add('pin--active'); // элементу на котором произошло событие добавляем класс pin--active
-  dialog.style.display = "block"; // открываем окно диалог 
-});
-
-
-//---закрываем окно диалог---//
+//---переменные для закрытия окна диалог---//
 var dialog = document.querySelector('.dialog'); // ищем окно диалог
 var dialogClose = document.querySelector('.dialog__close'); // ищем эемент для закрытия окна диалог
-var pinActive = document.querySelector('.pin--active'); // ищем элемент pin--active
+
+
+//---создаем цикл для отслеживания события на каждом элементе из коллекции с классом пин---//
+for (var i = 0; i < pinElements.length; i++) {
+  
+  pinElements[i].addEventListener('click', function () {
+
+    for (var j = 0; j < pinElements.length; j++) {
+      pinElements[j].classList.remove('pin--active'); // перебираем все элементы с классом pin и удаляем класс pin--active
+    }
+
+    this.classList.add('pin--active'); // элементу на котором произошло событие добавляем класс pin--active
+
+    dialog.style.display = "block"; // открываем окно диалог 
+    
+  });
+}
+
 
 //---закрываем окно диалог при клике на dialog__close ---//
 dialogClose.addEventListener('click', function () {
-  dialog.style.display = "none"; //добавляем окну диалог display: none
-  pinActive.classList.remove('pin--active'); // удаляем класс pin--active у элемента с классом pin--active
+  
+  dialog.style.display = "none"; //добавляем окну диалог свойство display: none
+  
+  for (var j = 0; j < pinElements.length; j++) {
+      pinElements[j].classList.remove('pin--active'); // перебираем все элементы с классом pin и удаляем класс pin--active
+    }
+  
 });
