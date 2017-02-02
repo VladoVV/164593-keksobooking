bf18252d01deaf36fa82f97e7dc8af29e9d50b16
VladoVV <wq2127@gmail.com>
2017-02-02 09:05:41 +0200

    Кексобукинг урок 3 задание1 увидел новые ошибки, опять исправил6

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@
 
 var pinElements = document.querySelectorAll('.pin'); //  выбираем все элементы с классом pin
 
-//  ---переменные для закрытия окна диалог--- // 
+//  ---переменные для закрытия окна диалог--- //
 var dialog = document.querySelector('.dialog'); //  ищем окно диалог
 var dialogClose = dialog.querySelector('.dialog__close'); //  ищем эемент для закрытия окна диалог
 
@@ -19,31 +19,31 @@ var typeApart = document.getElementById('type');
 var price = document.getElementById('price');
 
 
-//  перебираем все элементы с классом и удаляем класс// 
+//  перебираем все элементы с классом и удаляем класс//
 function removeClass(collection, className) {
   for (var j = 0; j < collection.length; j++) {
     collection[j].classList.remove(className);
   }
 }
 
-// ---создаем цикл для отслеживания события на каждом элементе из коллекции с классом пин---// 
+// ---создаем цикл для отслеживания события на каждом элементе из коллекции с классом пин---//
 for (var i = 0; i < pinElements.length; i++) {
   pinElements[i].addEventListener('click', function () {
     removeClass(pinElements, 'pin--active');
     this.classList.add('pin--active'); //  элементу на котором произошло событие добавляем класс pin--active
-    dialog.style.display = "block"; //  открываем окно диалог 
+    dialog.style.display = 'block'; //  открываем окно диалог
   });
 }
 
 
-// ---закрываем окно диалог при клике на dialog__close ---// 
+// ---закрываем окно диалог при клике на dialog__close ---//
 dialogClose.addEventListener('click', function () {
-  dialog.style.display = "none"; // добавляем окну диалог свойство display: none
+  dialog.style.display = 'none'; // добавляем окну диалог свойство display: none
   removeClass(pinElements, 'pin--active');
 });
 
 var prices = [1000, 0, 5000];
-// ---синхронизации данных между типом жилья и стоимостью (type & price)---// 
+// ---синхронизации данных между типом жилья и стоимостью (type & price)---//
 // изменяем стоимсть в зависимости от типа жилья
 typeApart.addEventListener('change', function () {
   price.min = price.value = prices[typeApart.selectedIndex];
@@ -52,20 +52,20 @@ typeApart.addEventListener('change', function () {
 
 // изменяем тип жилья в зависимости от стоимости
 price.addEventListener('change', function () {
-  if (price.value >= 1000 && price.value < 5000) {
+  if (price.value >= 1000 && price.value < 10000) {
     typeApart.selectedIndex = 0;
   }
   if (price.value === 0) {
     typeApart.selectedIndex = 1;
   }
-  if (price.value >= 5000) {
+  if (price.value >= 10000) {
     typeApart.selectedIndex = 2;
   }
 });
 
 
-// синхронизация данных между кол-вом комнат и кол-вом гостей (room_number & capacity)// 
-// ---функция проверки и синхронного изменения значения в списке capacity---// 
+// синхронизация данных между кол-вом комнат и кол-вом гостей (room_number & capacity)//
+// ---функция проверки и синхронного изменения значения в списке capacity---//
 roomNumber.addEventListener('change', function () {
   if (roomNumber.selectedIndex === 2) {
     capacity.selectedIndex = 1;
@@ -74,13 +74,13 @@ roomNumber.addEventListener('change', function () {
   }
 });
 
-// ---функция проверки и синхронного изменения значения в списке room_number---// 
+// ---функция проверки и синхронного изменения значения в списке room_number---//
 capacity.addEventListener('change', function () {
   roomNumber.selectedIndex = capacity.selectedIndex;
 });
 
 
-// ---синхронизация данных между временем заезда и временем выезда (time & timeout)---// 
+// ---синхронизация данных между временем заезда и временем выезда (time & timeout)---//
 // функция проверки и синхронного изменения значения в списке timeout
 timeSelect.addEventListener('change', function () {
   timeOutSelect.selectedIndex = timeSelect.selectedIndex;
