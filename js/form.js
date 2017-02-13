1d0842e589d0905d6538cce58e2976f2239fc648
VladoVV <wq2127@gmail.com>
2017-02-13 22:28:20 +0200

    1 задание сделал, 2 голову сломал, вроде должно работать

--- js/form.js
+++ js/form.js
@@ -6,11 +6,9 @@ var pinElements = document.querySelectorAll('.pin'); //  выбираем все
 var dialog = document.querySelector('.dialog'); //  ищем окно диалог
 var dialogClose = dialog.querySelector('.dialog__close'); //  ищем эемент для закрытия окна диалог
 
-// переменные для синхронизации данных между временем заезда и временем выезда (time & timeout)
-var timeSelect = document.getElementById('time'); //  получаем список вариантов для select time
-var timeOutSelect = document.getElementById('timeout'); //  получаем список вариантов для select timeout
 
-// переменные для синхронизации данных между кол-вом комнат и кол-вом гостей (time & timeout)
+
+// переменные для синхронизации данных между кол-вом комнат и кол-вом гостей (roomNumber & capacity)
 var roomNumber = document.getElementById('room_number');
 var capacity = document.getElementById('capacity');
 
@@ -50,52 +48,8 @@ function setValues() {
 }
 setValues();
 
-//  перебираем все элементы с классом и удаляем класс//
-function removeClass(collection, className) {
-  for (var j = 0; j < collection.length; j++) {
-    collection[j].classList.remove(className);
-  }
-}
+initializePins();
 
-// ---Делегируем для отслеживания события на каждом элементе из коллекции с классом пин---//
-var tokyoPinMap = document.querySelector('.tokyo__pin-map');
-
-var hzName = function (event) { // ХЗ как назвать эту функцию?
-  var target = event.target;
-  if (target.tagName === 'IMG') { // проверяем, если событие произошло на картинке, то
-    target = target.parentNode; // переопределяем таргет на родителя картинки (именно в заданной разметке)
-  }
-  removeClass(pinElements, 'pin--active');
-  target.classList.add('pin--active'); //  элементу на котором произошло событие добавляем класс pin--active
-  dialog.classList.remove('dialog-hidden'); //  открываем окно диалог
-  dialogClose.setAttribute('aria-pressed', 'true'); // добавляем атрибут тру, при открытии окна
-};
-
-tokyoPinMap.addEventListener('click', function (event) {
-  hzName(event);
-});
-
-tokyoPinMap.addEventListener('keydown', function (event) {
-  if (event.keyCode === ENTER_KEYCODE) {
-    hzName(event);
-  }
-});
-
-// ---закрываем окно диалог при клике на dialog__close ---//
-function closingDialog() {
-  dialog.classList.add('dialog-hidden'); // добавляем окну диалог свойство display: none
-  removeClass(pinElements, 'pin--active');
-  dialogClose.setAttribute('aria-pressed', 'false');
-}
-
-dialogClose.addEventListener('click', closingDialog);
-
-dialogClose.addEventListener('keydown', function (event) {
-  if (event.keyCode === ENTER_KEYCODE) {
-    closingDialog();
-    dialogClose.setAttribute('aria-pressed', 'false');
-  }
-});
 
 // ---синхронизации данных между типом жилья и стоимостью (type & price)---//
 // изменяем стоимсть в зависимости от типа жилья
@@ -120,27 +74,33 @@ price.addEventListener('change', function () {
 
 // синхронизация данных между кол-вом комнат и кол-вом гостей (room_number & capacity)//
 // ---функция проверки и синхронного изменения значения в списке capacity---//
-roomNumber.addEventListener('change', function () {
-  if (roomNumber.selectedIndex === 2) {
-    capacity.selectedIndex = 1;
-  } else {
-    capacity.selectedIndex = roomNumber.selectedIndex;
-  }
-});
+//roomNumber.addEventListener('change', function () {
+//  if (roomNumber.selectedIndex === 2) {
+//    capacity.selectedIndex = 1;
+//  } else {
+//    capacity.selectedIndex = roomNumber.selectedIndex;
+//  }
+//});
+//
+//// ---функция проверки и синхронного изменения значения в списке room_number---//
+//capacity.addEventListener('change', function () {
+//  roomNumber.selectedIndex = capacity.selectedIndex;
+//});
+//var synchronizeFields;
 
-// ---функция проверки и синхронного изменения значения в списке room_number---//
-capacity.addEventListener('change', function () {
-  roomNumber.selectedIndex = capacity.selectedIndex;
-});
-
-
-// ---синхронизация данных между временем заезда и временем выезда (time & timeout)---//
-// функция проверки и синхронного изменения значения в списке timeout
-timeSelect.addEventListener('change', function () {
-  timeOutSelect.selectedIndex = timeSelect.selectedIndex;
-});
-
-// функция проверки и синхронного изменения значения в списке time
-timeOutSelect.addEventListener('change', function () {
-  timeSelect.selectedIndex = timeOutSelect.selectedIndex;
-});
+synchronizeFields('time', 'timeout', ['12', '13', '14'], ['12', '13', '14'], 'value');
+//synchronizeFields('room_number', 'capacity', ['1 комната', '2 комнаты', '100 комнат'], ['не для гостей', 'для 3 гостей', 'для 3 гостей'], 'value');
+//// переменные для синхронизации данных между временем заезда и временем выезда (time & timeout)
+//var timeSelect = document.getElementById('time'); //  получаем список вариантов для select time
+//var timeOutSelect = document.getElementById('timeout'); //  получаем список вариантов для select timeout
+//
+//// ---синхронизация данных между временем заезда и временем выезда (time & timeout)---//
+//// функция проверки и синхронного изменения значения в списке timeout
+//timeSelect.addEventListener('change', function () {
+//  timeOutSelect.selectedIndex = timeSelect.selectedIndex;
+//});
+//
+//// функция проверки и синхронного изменения значения в списке time
+//timeOutSelect.addEventListener('change', function () {
+//  timeSelect.selectedIndex = timeOutSelect.selectedIndex;
+//});
