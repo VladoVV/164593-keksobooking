637db2f86f4bc69d8b38190ba780711156727da2
VladoVV <wq2127@gmail.com>
2017-02-22 15:14:23 +0200

    incapul

--- js/form.js
+++ js/form.js
@@ -1,47 +1,48 @@
 'use strict';
+(function () {
+  var initializePins = window.initializePins;
+  var synchronizeFields = window.synchronizeFields;
 
-var initializePins = window.initializePins;
-var synchronizeFields = window.synchronizeFields;
+  // переменные для синхронизации данных между типом жилья и стоимостью (type & price)
 
-// переменные для синхронизации данных между типом жилья и стоимостью (type & price)
+  var price = document.getElementById('price');
+  var maxValuePrice = 1000000;
+  var minValuePrice = 1000;
+  var titleAd = document.getElementById('title');
+  var titleMinLength = 30;
+  var titleMaxLength = 100;
+  var adressApart = document.getElementById('address');
+  var times = ['После 12', 'После 13', 'После 14'];
+  var timeOuts = ['Выезд до 12', 'Выезд до 13', 'Выезд до 14'];
+  var roomNumbers = ['1 комната', '2 комнаты', '100 комнат'];
+  var capacities = ['не для гостей', 'для 3 гостей', 'для 3 гостей'];
+  var typesOfApartment = ['Квартира', 'Лачуга', 'Дворец'];
+  var prices = ['1000', '0', '10000'];
 
-var price = document.getElementById('price');
-var maxValuePrice = 1000000;
-var minValuePrice = 1000;
-var titleAd = document.getElementById('title');
-var titleMinLength = 30;
-var titleMaxLength = 100;
-var adressApart = document.getElementById('address');
-var times = ['После 12', 'После 13', 'После 14'];
-var timeOuts = ['Выезд до 12', 'Выезд до 13', 'Выезд до 14'];
-var roomNumbers = ['1 комната', '2 комнаты', '100 комнат'];
-var capacities = ['не для гостей', 'для 3 гостей', 'для 3 гостей'];
-var typesOfApartment = ['Квартира', 'Лачуга', 'Дворец'];
-var prices = ['1000', '0', '10000'];
+  // задаем ограничения для полей формы
+  function setValues() {
+    price.required = true;
 
-// задаем ограничения для полей формы
-function setValues() {
-  price.required = true;
+    price.addEventListener('blur', function maxMinPrice() {
+      if (price.value > maxValuePrice) {
+        price.value = maxValuePrice;
+      }
+      if (price.value < minValuePrice) {
+        price.value = minValuePrice;
+      }
+    });
 
-  price.addEventListener('blur', function maxMinPrice() {
-    if (price.value > maxValuePrice) {
-      price.value = maxValuePrice;
-    }
-    if (price.value < minValuePrice) {
-      price.value = minValuePrice;
-    }
-  });
+    titleAd.minLength = titleMinLength;
+    titleAd.maxLength = titleMaxLength;
+    titleAd.required = true;
 
-  titleAd.minLength = titleMinLength;
-  titleAd.maxLength = titleMaxLength;
-  titleAd.required = true;
+    adressApart.required = true;
+  }
+  setValues();
 
-  adressApart.required = true;
-}
-setValues();
+  initializePins();
 
-initializePins();
-
-synchronizeFields('time', 'timeout', times, timeOuts, 'value');
-synchronizeFields('room_number', 'capacity', roomNumbers, capacities, 'value');
-synchronizeFields('type', 'price', typesOfApartment, prices, 'value');
+  synchronizeFields('time', 'timeout', times, timeOuts, 'value');
+  synchronizeFields('room_number', 'capacity', roomNumbers, capacities, 'value');
+  synchronizeFields('type', 'price', typesOfApartment, prices, 'value');
+})();
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,12 +1,13 @@
 'use strict';
+(function () {
+  window.synchronizeFields = function (element1, element2, values1, values2, syncProperty) {
 
-window.synchronizeFields = function (element1, element2, values1, values2, syncProperty) {
+    document.getElementById(element1).addEventListener('change', function () {
+      document.getElementById(element2)[syncProperty] = values2[values1.indexOf(document.getElementById(element1)[syncProperty])];
+    });
 
-  document.getElementById(element1).addEventListener('change', function () {
-    document.getElementById(element2)[syncProperty] = values2[values1.indexOf(document.getElementById(element1)[syncProperty])];
-  });
-
-  document.getElementById(element2).addEventListener('change', function () {
-    document.getElementById(element1)[syncProperty] = values1[values2.indexOf(document.getElementById(element2)[syncProperty])];
-  });
-};
+    document.getElementById(element2).addEventListener('change', function () {
+      document.getElementById(element1)[syncProperty] = values1[values2.indexOf(document.getElementById(element2)[syncProperty])];
+    });
+  };
+})();
